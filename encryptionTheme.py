78401ed42686043189ffef6b99d56f9faad502ff
Jonathan McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2021-03-12 22:12:52 -0600

    Add files via upload

---
+++ encryptionTheme.py
@@ -0,0 +1,42 @@
+#Jonathan McCormick
+#Outsmart Big Brother (TM)
+#12 March, 2021
+
+# E N C R Y P T I O N 
+
+youWantToOutsmartBigBrother = True
+
+def encryptedEmail():
+    print("Use encrypted email,")
+
+def encryptedChat():
+    print("Use encrypted chat,")
+
+def https():
+    print("Make sure your website connections are encrypted using https,")
+
+def vpn():
+    print("Use a VPN,")
+
+def encryptedHardDrive():
+    print("When configuring a Linux distro, encrypt your hard drive behind a second password (if you can handle it),")
+
+def keepLearning():
+    print("Keep learning! :)")
+
+def encryptEverything():
+    encryptedEmail()
+    https()
+    vpn()
+    encryptedHardDrive()
+    keepLearning()
+
+def beVulnerable():
+    print("Your data is vulnerable.")
+
+if youWantToOutsmartBigBrother == True:
+    print("Since you want to Outsmart Big Brother, \n")
+    encryptEverything()
+
+else:
+    beVulnerable()
