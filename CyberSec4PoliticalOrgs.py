44ec447c290e379f44390ac00bb2a579f7305e94
Jonathan McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2021-03-25 15:13:28 -0500

    Create CyberSec4PoliticalOrgs.py
    Python summary of content at https://outsmartbigbrother.com/2021/03/21/cybersecurity-for-startup-political-organizations-and-news-outlets/

---
+++ CyberSec4PoliticalOrgs.py
@@ -0,0 +1,66 @@
+#Jonathan McCormick
+#Outsmart Big Brother (TM)
+#25 March, 2021
+
+# C Y B E R S E C U R I T Y   4   P O L I T I C A L   O R G A N I Z A T I O N S 
+
+youWantToOutsmartBigBrother = True
+
+def useStrongPassphrases():
+    print("Use long, diverse, original, and well-protected passphrases,")
+
+def twoFactorAuth():
+    print("Use two-factor authentication,")
+
+def https():
+    print("Make sure your website connections are encrypted using https,")
+
+def vpn():
+    print("Use a VPN,")
+
+def antivirusSoftware():
+    print("Use antivirus software,")
+  
+def haveEmailFilters():
+  print("Make sure your email account has spam filters,")
+  
+def limitPermissions():
+    print("Limit your employees' permissions to only what is necessary and proper, but provide controlled opportunities for talent growth,")
+    
+def updateAndUpgradeSoftware():
+    print("Update and upgrade your software regularly,")
+    
+def keepPrivateInfoPrivate():
+    print("Do not needlessly disclose your sensitive information to anyone,")
+    
+def backupAndEncrypt():
+    print("Backup and encrypt your data,")
+    
+def overcomeHumanError():
+    print("Have procedures and policies in place to overcome the natural, human tendency to err,")
+
+def keepLearning():
+    print("Keep learning! :)")
+
+def beSmart():
+    useStrongPassphrases()
+    twoFactorAuth()
+    https()
+    vpn()
+    antivirusSoftware()
+    haveEmailFilters()
+    limitPermissions()
+    updateAndUpgradeSoftware()
+    keepPrivateInfoPrivate()
+    backupAndEncrypt()
+    keepLearning()
+
+def beVulnerable():
+    print("Your data is vulnerable.")
+
+if youWantToOutsmartBigBrother == True:
+    print("Since you want to Outsmart Big Brother, \n")
+    beSmart()
+
+else:
+    beVulnerable()
